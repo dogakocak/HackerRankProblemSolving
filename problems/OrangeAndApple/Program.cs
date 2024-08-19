632f672658c46515776e18dd0d4bb15e30b0d3eb
Doğa Koçak <doga_kocak0623@hotmail.com>
2024-08-19 13:52:58 +0300

    added OrangeAndApple problem
    Solved the problem in one loop

---
+++ problems/OrangeAndApple/Program.cs
@@ -0,0 +1,44 @@
+﻿class Result
+{
+
+    public static void countApplesAndOranges(int s, int t, int a, int b, List<int> apples, List<int> oranges)
+    {
+        int appleCounter = 0;
+        int orangeCounter = 0;
+
+        //I'm trying to solve the problem in a single loop
+        for (int i = 0; i < Math.Max(apples.Count, oranges.Count); i++)
+        {
+            if(i < apples.Count)
+            {
+                if (apples[i]+a >= s && apples[i]+a <= t)
+                {
+                    appleCounter++;
+                }
+
+            }
+            if (i < oranges.Count)
+            {
+                if (oranges[i]+b >= s && oranges[i]+b <= t )
+                {
+                    orangeCounter++;
+                }
+
+            }
+
+        }
+        Console.WriteLine(appleCounter);
+        Console.WriteLine(orangeCounter);
+    }
+
+}
+
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        Result.countApplesAndOranges(s: 2, t: 8, a: 1, b: 10, apples: [3, -2, 5], oranges: [2,2]);
+
+    }
+}
